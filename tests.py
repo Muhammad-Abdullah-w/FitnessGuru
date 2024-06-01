56fab5e4f36d8d89574c99ec120de08d10e8f2d9
Muhammad-Abdullah-w <59945909+Muhammad-Abdullah-w@users.noreply.github.com>
2024-06-01 10:45:57 +0500

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.chrome import ChromeDriverManager
@@ -14,24 +15,31 @@ class FreshWorksTest(unittest.TestCase):
         elif browser.lower() == 'firefox':
             self.driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
         
-        self.driver.implicitly_wait(5)
-        self.driver.get("https://www.freshworks.com/")
+        #self.driver.implicitly_wait(5)
+        #self.driver.get("https://www.freshworks.com/")
 
     def test_freshWorksLogo(self):
-        flag = self.driver.find_element(By.CSS_SELECTOR, "a.logo.logo-fworks").is_displayed()
-        self.assertTrue(flag)
+        try:
+            flag = self.driver.find_element(By.CSS_SELECTOR, "a.logo.logo-fworks").is_displayed()
+            self.assertTrue(flag, "FreshWorks logo is not displayed on the homepage.")
+        except NoSuchElementException:
+            self.fail("FreshWorks logo element not found on the homepage.")
 
     def test_freshWorksTitle(self):
         print("running title test...")
-        title = self.driver.get_title()
+        title = self.driver.title
         print(title)
         self.assertEqual(title, "A fresh approach to customer engagement")
 
     def test_getFooterLinks(self):
-        footer_links_list = self.driver.find_elements(By.CSS_SELECTOR, "ul.footer-nav li a")
-        for ele in footer_links_list:
-            print(ele.text)
-        self.assertEqual(len(footer_links_list), 35)
+        # Verify if the footer contains the expected number of links
+        try:
+            footer_links_list = self.driver.find_elements(By.CSS_SELECTOR, "#footer a.footera")
+            for ele in footer_links_list:
+                print(ele.text)
+            self.assertEqual(len(footer_links_list), 35, "Footer links count mismatch.")
+        except NoSuchElementException:
+            self.fail("Footer links not found.")
 
     def tearDown(self):
         self.driver.quit()
